c17410d4352b70476019e019a693b6e2c9cd27e2
rutvik <rutvikpadhiyar000@gmail.com>
2021-06-14 11:29:13 +0530

    Added basic html-css

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import DEBUG
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,4 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     context = {}
-    return render_template('home.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
